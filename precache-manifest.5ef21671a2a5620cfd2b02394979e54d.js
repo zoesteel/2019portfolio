5ee1de0d9d19967932e2d9b05c0fa46232ce6585
zoesteel <zoeysteel@gmail.com>
2019-08-28 22:00:54 -0700

    gh-pages

---
+++ precache-manifest.5ef21671a2a5620cfd2b02394979e54d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f91ac4672a4fea7c605b334e532da738",
+    "url": "/2019portfolio/index.html"
+  },
+  {
+    "revision": "bad1b97926cf29e06654",
+    "url": "/2019portfolio/static/css/main.a8bf188d.chunk.css"
+  },
+  {
+    "revision": "6dd41cb16e754c17588c",
+    "url": "/2019portfolio/static/js/2.c07deee3.chunk.js"
+  },
+  {
+    "revision": "bad1b97926cf29e06654",
+    "url": "/2019portfolio/static/js/main.84b1763a.chunk.js"
+  },
+  {
+    "revision": "d0c92f0b681d4f765e84",
+    "url": "/2019portfolio/static/js/runtime~main.3c3e68bd.js"
+  }
+]);
